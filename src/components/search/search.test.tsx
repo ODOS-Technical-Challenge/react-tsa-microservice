5b0df1aa71cfbe99eff27cf7624f0c0fb30f8b2f
Clayton Miller <cmiller@synergybis.com>
2022-01-26 18:33:41 -0500

    added unit tests

--- src/components/search/search.test.tsx
+++ src/components/search/search.test.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { SearchPage } from "./search.page";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 
+import { SearchPage } from "./search.page";
+
 describe("Application Page: Search Page", () => {
-  it("should handle rendering the component.", () => {
+  it("should handle rendering the component.", async () => {
     render(
       <BrowserRouter>
         <SearchPage />
       </BrowserRouter>
     );
 
-    const search = screen.findByRole("textbox");
+    const search = await screen.findByLabelText("Search");
     expect(search).toBeInTheDocument;
+  });
+
+  it("should handle user interaction: 'search' action.", async () => {
+    render(
+      <BrowserRouter>
+        <SearchPage />
+      </BrowserRouter>
+    );
+
+    const search = await screen.findByLabelText("Search");
+    userEvent.type(search, "query");
+
+    const button = await screen.findByRole("button");
+    userEvent.click(button);
   });
 });
--- src/hooks/airport.test.ts
+++ src/hooks/airport.test.ts
@@ -1,10 +1,26 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { useAirport } from "./airport.hook";
+import { getAirportChecks } from "../api/airport.api";
+
+jest.mock("../api/airport.api", () => ({
+  getAirportChecks: jest.fn(),
+}));
+
+const mockedFunction = getAirportChecks as jest.MockedFunction<
+  typeof getAirportChecks
+>;
 
 describe("Hook: Use Airport", () => {
   it("should handle being called.", () => {
     const { result } = renderHook(() => useAirport());
 
     expect(result.current.isLoading).toBeFalsy();
+  });
+
+  it("should handle being called with query.", () => {
+    mockedFunction.mockResolvedValue({ data: [], status: 200 });
+    const { result } = renderHook(() => useAirport("Dulles"));
+
+    expect(result.current.isLoading).toBeTruthy();
   });
 });
